530494c1cc83b74e53fd0bb9bab1ca363899d8ad
Michal Fuhrmann <torosmg@gmail.com>
2015-12-12 22:05:20 +0100

    session working

--- battleships/user_services.py
+++ battleships/user_services.py
@@ -1,11 +1,28 @@
 import hashlib, uuid
 from battleships.models import User
 
+# key - hash, value - username
+SESSIONS = {}
+
+
+def create_session(username):
+    salt = uuid.uuid4().hex
+    string = username + salt
+    string = string.encode('utf-8')
+    user_session = hashlib.sha256(string).hexdigest()
+    SESSIONS[user_session] = username
+    return user_session
+
+
+def get_logged_user(user_session):
+    return SESSIONS.get(user_session)
+
 
 def hash_password(password, salt=uuid.uuid4().hex):
     string = password + salt
     string = string.encode('utf-8')
     return salt, hashlib.sha256(string).hexdigest()
+
 
 def user_login(username, password):
     user = User.objects.filter(username=username)
@@ -15,6 +32,7 @@ def user_login(username, password):
         if user.password == _password:
             return user
     return None
+
 
 def user_register(username, password):
     if User.objects.filter(username=username):
--- battleships/views.py
+++ battleships/views.py
@@ -1,29 +1,41 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader, Context
-from django.shortcuts import render_to_response, render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-from battleships.user_services import user_login, user_register
+from battleships.user_services import user_login, user_register, create_session, get_logged_user
+
+COOKIE_SESSION = 'session-cookie'
+
+
+def my_render(request, template, variables={}):
+    user_session = request.COOKIES.get(COOKIE_SESSION)
+    if user_session:
+        username = get_logged_user(user_session)
+        variables['app_user'] = username if username else 'gosc'
+    return render(request, template, variables)
 
 
 def index(request):
-    return render(request, "battleships/user/user.html")
+    return my_render(request, "battleships/user/user.html")
 
 
 def login(request):
     if request.method == 'GET':
-        return render(request, "battleships/user/login.html")
+        return my_render(request, "battleships/user/login.html")
     elif request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
         if user_login(username, password):
-            return HttpResponseRedirect('/battleships/main')
-        return render(request, "battleships/user/login.html", {"error": 'Zly login/haslo'})
+            user_session = create_session(username)
+            response = HttpResponseRedirect('/battleships/main')
+            response.set_cookie(COOKIE_SESSION, user_session)
+            return response
+        return my_render(request, "battleships/user/login.html", {"error": 'Zly login/haslo'})
 
 
 def register(request):
     if request.method == 'GET':
-        return render(request, "battleships/user/register.html")
+        return my_render(request, "battleships/user/register.html")
     elif request.method == 'POST':
         username = request.POST['username']
         password1 = request.POST['password1']
@@ -35,8 +47,10 @@ def register(request):
             if user_register(username, password1):
                 return HttpResponseRedirect('/battleships/login')
             error = 'Login zajety'
-        return render(request, "battleships/user/register.html", {"error": error})
+        return my_render(request, "battleships/user/register.html", {"error": error})
 
 
 def main(request):
-    return render(request, "battleships/user/main.html")
+    return my_render(request, "battleships/user/main.html")
+
+
